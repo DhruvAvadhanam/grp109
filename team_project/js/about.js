1d760a40b158eb0a301af9d7cc43fb81d048214c
Ia-Fin <ian.finley@bellevuecollege.edu>
2025-05-17 23:08:48 +0000

    Updated IF statements to SWITCH
    The js was failing to load in the staff and bio using the IF and ELSE IF statements. Changing to SWITCH appears to have fixed it.

--- team_project/js/about.js
+++ team_project/js/about.js
@@ -1,33 +1,36 @@
-var staff = ""
-
-var bio = ""
-function displayStaff() {
-      if staff = 'Yuval'{
-        document.getElementById("Yuval").value;
-        staff = "Yuval Amitay"
-        var bio = ""
-    }  else if {
-        document.getElementById("Ian").value;
-        staff = "Ian Finley"
-        var bio = ""
-    }  else if {
-        document.getElementById("Dhruv").value;
-        staff = "Dhruv Adnamhanam"
-        var bio = ""
-    }  else if {
-        document.getElementById("Deena").value; 
-        staff = "Deena Randolph"
-        var bio = "Deena Randolph has a background in IT Helpdesk Support and is studying Software Development here at BC. She has taken web development and programming courses previously, knows just enough random information to be dangerous, and relies on her valiant coworkers to keep the website running."
-    } else 
-          document.getElementById("default").value;
-          staff = "Please select a staff member"
-    }
-    text = '<h2>' + staff + '</h2>' +'<p>' + bio + '</p>';
-}
-
 window.onload = function () {
-  document.getElementById("staff").onchange = displayStaff;  
-  displaySelectedValue();
-//print text to blackboard
-var el = document.getElementById('feature')
-el.innerHTML = text
+  document.getElementById("staff").onchange = displayStaff;
+  displayStaff(); // ensures the default message is shown on load
+};
+
+function displayStaff() {
+  const selected = document.getElementById("staff").value;
+  let staff = "";
+  let bio = "";
+
+  switch (selected) {
+    case "Yuval":
+      staff = "Yuval Amitay";
+      bio = "";
+      break;
+    case "Ian":
+      staff = "Ian Finley";
+      bio = "Ian Finley has a work background as a senior technical support engineer, and is studying for the Data Reporting Analyst Certificate of Accomplishment at Bellevue College. While his educational background was media arts and animation, he has pivoted towards technical careers and largely leaves visual arts endevours to his hobbies.";
+      break;
+    case "Dhruv":
+      staff = "Dhruv Avadhanam";
+      bio = "";
+      break;
+    case "Deena":
+      staff = "Deena Randolph";
+      bio = "Deena Randolph has a background in IT Helpdesk Support and is studying Software Development here at BC. She has taken web development and programming courses previously, knows just enough random information to be dangerous, and relies on her valiant coworkers to keep the website running.";
+      break;
+    default:
+      staff = "Please select a staff member";
+      bio = "";
+      break;
+  }
+
+  const text = '<h2>' + staff + '</h2><p>' + bio + '</p>';
+  document.getElementById("feature").innerHTML = text;
+}
