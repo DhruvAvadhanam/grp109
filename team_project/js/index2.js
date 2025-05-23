ae7ad67827552946e96dff7eb2b88b315f789927
DhruvAvadhanam <1042772@lwsd.org>
2025-05-23 11:30:29 -0700

    Update index2.js
    made user typing input to filter classes

--- team_project/js/index2.js
+++ team_project/js/index2.js
@@ -1,27 +1,27 @@
 function displayFilteredClasses() {
-  var cuisine = document.getElementById("cuisines").value;
-  var day = document.getElementById("days").value;
+  var name = document.getElementById("className").value.toLowerCase();
   var cards = document.getElementsByClassName("class-card");
-  
+  var includes = false;
   for (var i = 0; i < cards.length; i++) {
     var card = cards [i];
-    var cardCuisine = card.getAttribute("data-cuisine");
-    var cardDay = card.getAttribute("data-day");
+    var cardName = card.getAttribute("data-name").toLowerCase();
+    var nameMatch = cardName.includes(name);
 
-    var cuisineMatch = (cuisine === "All" || cardCuisine === cuisine);
-    var dayMatch = (day === "All" || cardDay === day);
-
-    if (cuisineMatch && dayMatch) {
+    if (nameMatch) {
       card.style.display = "block";
+      includes = true;
     } else {
       card.style.display = "none";
     }
   }
+  if (!includes){
+    noResults.style.display = "block";
+  } else {
+    noResults.style.display = "none";
+  }
 }
 
 window.onload = function () {
-  document.getElementById("cuisines").onchange = displayFilteredClasses;
-  document.getElementById("days").onchange = displayFilteredClasses;
-  
+  document.getElementById("className").addEventListener("input", displayFilteredClasses);  
   displaySelectedValue();
 };
