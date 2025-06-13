dd6dc4f938b35a1b7360f2212466fd3a66c8c70b
DhruvAvadhanam <1042772@lwsd.org>
2025-06-13 15:37:15 -0700

    Update account_page.js

--- team_project/js/account_page.js
+++ team_project/js/account_page.js
@@ -1,6 +1,4 @@
-// This function will run when "Create Account" is clicked
 document.getElementById("createAccount").addEventListener("click", function () {
-  // Redirect to a new page — replace 'new_user_dashboard.html' with your actual future page
   window.location.href = "createAccount.html";
 });
 
