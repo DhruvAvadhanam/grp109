4a82077c28217ad7ea129a59203c29646bd9fcc6
Ia-Fin <ian.finley@bellevuecollege.edu>
2025-06-20 03:25:44 +0000

    Testing a fix with conflicting showslides definitions

--- team_project/js/carousel.js
+++ team_project/js/carousel.js
@@ -1,21 +1,5 @@
 let slideIndex = 1;
-showSlides();
-// Show slide function with out current page paramenter on automatic loop.
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    /*slides[i].style.display = "none";*/
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
+showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -26,20 +10,28 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-// Show slide function with current slide parameter.
+
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 8000); // Change image every 4 seconds
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+
+// Auto-loop (separate from above)
+setInterval(() => {
+  plusSlides(1);
+}, 8000);
